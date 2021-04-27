8e727a13999bce299c7d615755a71c4774902793
Alexis Rodriguez <76778611+Alexisgr93@users.noreply.github.com>
2021-04-27 19:51:52 -0300

    Se realizaron modificaciones de index y mas...
    Cree un nuevo index principal, el index anterior que solia ser se llama menu dentro del cual se visualiza con un nuevo formato de imágenes en el body, el actual index esta incompleto solo se creo para no tenerlo en cuenta cuando se tenga que realizar el registro o el ingreso del usuario, luego medique los archivos css para tenerlos separados en caso de que se agregue información de otro html, asi no surge inconvenientes, estoy corrigiendo los html por el cual no deshabilite el js el cual surge molesto a la hora de poner el archivo en marcha y buscar error.

---
+++ js/acciones-js/menu.js
@@ -0,0 +1,92 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}// JavaScript Document
+
+
+function frases_oculta(id){
+if (document.getElementById){ //se obtiene el id
+var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+}
+}
+window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+frases_oculta('frases_a_mostrar');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+}
+
+/*
+ (function (){
+	var content = document.getElementById("geolocation-test");
+
+	if (navigator.geolocation)
+	{
+		navigator.geolocation.getCurrentPosition(function(objPosition)
+		{
+			var lon = objPosition.coords.longitude;
+			var lat = objPosition.coords.latitude;
+			var cor = objPosition.coords.accuracy;
+			
+			var las = -58.4850904;
+			var los =  -34.6757411;
+		if(objPosition.coords.longitude==las){
+		alert("si");
+		}
+		/*Muestra la latitud en pantalla
+			content.innerHTML = "<p><strong>Latitud:</strong> " + lat + "</p><p><strong>Longitud:</strong> " + lon + "</p>";*/
+	/*		
+		var	rad = function(x) {return x*Math.PI/180;}
+  var R = 6378.137; //Radio de la tierra en km 
+  var dLat = rad( las - objPosition.coords.latitude );
+  var dLong = rad( los - objPosition.coords.longitude );
+  
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(objPosition.coords.latitude)) * 
+  Math.cos(rad(las)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+  
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  //aquí obtienes la distancia en metros por la conversion 1Km =1000m
+  var d = (R * c); 
+  alert( d.toFixed(3)) ; 
+			
+			
+			
+			
+			
+			confirm("<p><strong>Latitud:</strong> " + lat + "</p><p><strong>Longitud:</strong> " + lon + "</p>" +  "Usted se encuentra a:" + cor.toFixed(0) + " metros");
+
+		}, function(objPositionError)
+		{
+			
+				
+			switch (objPositionError.code)
+			{
+				case objPositionError.PERMISSION_DENIED:
+					confirm("No se ha permitido el acceso a la posición del usuario.");
+				break;
+				case objPositionError.POSITION_UNAVAILABLE:
+					confirm("No se ha podido acceder a la información de su posición.");
+				break;
+				case objPositionError.TIMEOUT:
+					confirm("El servicio ha tardado demasiado tiempo en responder.");
+				break;
+				default:
+					confirm("Error desconocido.");
+					
+				
+			}
+		}, {
+			maximumAge: 75000,
+			timeout: 15000
+		});
+	}
+	else
+	{
+		content.innerHTML = "Su navegador no soporta la API de geolocalización.";
+	}
+})();
+
+*/
